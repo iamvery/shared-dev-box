dd17fc84f8bbd8cb3dc904f0f9e91dcd2cb3690e
Jay Hayes <jay.hayes@dynetics.com>
2013-01-24 22:56:37 -0600

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant::Config.run do |config|
+  config.vm.box = 'precise32'
+  config.vm.box_url = 'http://files.vagrantup.com/precise32.box'
+  config.vm.host_name = 'shared-dev-box'
+
+  config.vm.provision :puppet,
+    :manifests_path => 'puppet/manifests',
+    :module_path    => 'puppet/modules'
+
+  # Forward a port from the guest to the host, which allows for outside
+  # computers to access the VM, whereas host only networking does not.
+  # config.vm.forward_port 80, 8080
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,47 @@
+# Make sure apt-get -y update runs before anything else.
+stage { 'preinstall':
+  before => Stage['rvm-install']
+}
+
+class apt_get_update {
+  exec { '/usr/bin/apt-get -y update':
+    user => 'root'
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+class install_core_packages {
+  if !defined(Package['build-essential']) {
+    package { 'build-essential':
+      ensure => installed
+    }
+  }
+
+  if !defined(Package['git-core']) {
+    package { 'git-core':
+      ensure => installed
+    }
+  }
+}
+class { 'install_core_packages': }
+
+class install_rvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  rvm_system_ruby {
+    'ruby-1.9.3-p374':
+      ensure => 'present',
+      default_use => true;
+  }
+
+  rvm_gem {
+    'ruby-1.9.3-p374@default/puppet':
+      ensure => '3.0.2',
+      require => Rvm_system_ruby['ruby-1.9.3-p374'];
+  }
+}
+
+class { 'install_rvm': }
